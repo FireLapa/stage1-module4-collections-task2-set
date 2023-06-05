b1fa3a686370b8e8172cf3a54f536e58cb94e1f4
Lapa <lapabig@gmail.com>
2023-06-05 14:23:24 +0600

    Updated

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,21 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int x : sourceList) {
+            hashSet.add(x);
+
+            if (x % 2 == 0) {
+                int temp = x / 2;
+                while (temp % 2 == 0) {
+                    hashSet.add(temp);
+                    temp = temp / 2;
+                }
+                hashSet.add(temp);
+            } else {
+                hashSet.add(2 * x);
+            }
+        }
+        return hashSet;
     }
 }
